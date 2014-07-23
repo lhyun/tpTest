2923a8c5a3af1a49cb0cf1f57593e664ba1623fa
lhyun <hyl54372@gmail.com>
2014-07-23 14:05:14 +0800

    Revert "Revert "thinphp学习""
    This reverts commit 33cc4bfb3475b48e92dffa44944c9864689f5397.

---
+++ CosWall/Common/Conf/config.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+	'APP_DEBUG' => true,
+	'DB_TYPE'   => 'mysql', // 数据库类型
+	'DB_HOST' => '127.0.0.1',
+	'DB_USER' => 'root',
+	'DB_PWD' => '19930123lwy',
+	'DB_PORT'   => 3306, // 端口
+	'DB_NAME' => 'information_schema',
+	'URL_MODEL' => 1
+);
